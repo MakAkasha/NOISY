05912d419be92cbe69ddbd96765a8ccb7ac71df8
ElMubark A.Elwahab Hassan <Mobark.k11@outlook.com>
2022-03-14 19:14:46 +0200

    add survey

---
+++ php/mailer.php
@@ -0,0 +1,47 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+require './mailer/src/Exception.php';
+require './mailer/src/PHPMailer.php';
+require './mailer/src/SMTP.php';
+
+$mail = new PHPMailer;
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'ssl://noisy.sd';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;    
+// $mail->SMTPDebug = 2;                           // Enable SMTP authentication
+$mail->Username = 'contact@noisy.sd';                 // SMTP username
+$mail->Password = 'contact123456';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+
+$mail->From = 'contact@noisy.sd';
+$mail->FromName = 'Noisy Contact Sender';
+$mail->addAddress('mobark.k9@gmail.com', 'Mobark');     // Add a recipient
+// $mail->addAddress('ellen@example.com');               // Name is optional
+// $mail->addReplyTo('info@example.com', 'Information');
+// $mail->addCC('cc@example.com');
+// $mail->addBCC('bcc@example.com');
+
+// $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
+// $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Here is the subject';
+$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
+?>
